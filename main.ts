e8e1b8d1e1e318182de508641c5f21ebda606131
GURBEY DEMIRTURK <gurbey61@gmail.com>
2023-02-08 21:21:34 -0600

    If condition utilizes light level

--- main.ts
+++ main.ts
@@ -1,24 +1,3 @@
-// ON BUTTON A PRESSED
-// Text "GETREADY!" scrolls
-// For Loops starts a count down from 
-// 3 and shows each number
-// Then a jumping jack image appears on the screen
-input.onButtonPressed(Button.A, function () {
-    basic.showString("GETREADY!")
-    for (let index = 0; index <= index; index++) {
-        if (3 - index > 0) {
-            basic.showNumber(3 - index)
-        } else {
-            images.createBigImage(`
-                # . # . # . . # . .
-                . # # # . . # # # .
-                . . # . . # . # . #
-                . # . # . . # . # .
-                # . . . # . # . # .
-                `).scrollImage(5, 400)
-        }
-    }
-})
 // Rubric
 // - Part of the message scrolls
 // - Uses letters, numbers, or symbols
@@ -27,82 +6,93 @@ input.onButtonPressed(Button.A, function () {
 // 
 // ON START 
 // An Arrow shape slowly appears from X4Y2 by scrolling towards Button A. 
-for (let index = 0; index < 1; index++) {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . #
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . #
-        . . . # .
-        . . . . #
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . #
-        . . . # .
-        . . # . #
-        . . . # .
-        . . . . #
-        `)
-    basic.showLeds(`
-        . . . # .
-        . . # . .
-        . # . # #
-        . . # . .
-        . . . # .
-        `)
-    basic.showLeds(`
-        . . # . .
-        . # . . .
-        # . # # #
-        . # . . .
-        . . # . .
-        `)
-    basic.showLeds(`
-        . # . . .
-        # . . . .
-        . # # # .
-        # . . . .
-        . # . . .
-        `)
-    basic.showLeds(`
-        # . . . .
-        . . . . .
-        # # # . .
-        . . . . .
-        # . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        # # . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        # . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-}
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . #
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . # .
+    . . . . #
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . #
+    . . . # .
+    . . # . #
+    . . . # .
+    . . . . #
+    `)
+basic.showLeds(`
+    . . . # .
+    . . # . .
+    . # . # #
+    . . # . .
+    . . . # .
+    `)
+basic.showLeds(`
+    . . # . .
+    . # . . .
+    # . # # #
+    . # . . .
+    . . # . .
+    `)
+basic.showLeds(`
+    . # . . .
+    # . . . .
+    . # # # .
+    # . . . .
+    . # . . .
+    `)
+basic.showLeds(`
+    # . . . .
+    . . . . .
+    # # # . .
+    . . . . .
+    # . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    # # . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.forever(function () {
+    if (input.lightLevel() > 128) {
+        basic.showString("GETREADY!")
+    } else {
+        images.createBigImage(`
+            # . # . # . . # . .
+            . # # # . . # # # .
+            . . # . . # . # . #
+            . # . # . . # . # .
+            # . . . # . # . # .
+            `).scrollImage(5, 400)
+    }
+})
